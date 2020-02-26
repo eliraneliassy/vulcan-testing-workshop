9b90f408a8dcc56440263f4e491d5178f2789829
Eliran Eliassy <eliran.eliassy@gmail.com>
2020-02-26 14:14:29 +0200

    test directives

--- src/app/change-color.directive.spec.ts
+++ src/app/change-color.directive.spec.ts
@@ -1,8 +1,50 @@
 import { ChangeColorDirective } from './change-color.directive';
+import { Component, DebugElement } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
+@Component({
+  template: `
+  <div appChangeColor>color should be yellow</div>
+  <div >no bg color</div>
+  `
+})
+class TestComponent { }
 
 describe('ChangeColorDirective', () => {
+
+  let fixture: ComponentFixture<TestComponent>;
+  let component: TestComponent;
+  let el: DebugElement;
+
+  beforeEach(async(() => {
+
+    TestBed.configureTestingModule({
+
+      declarations: [
+        TestComponent, ChangeColorDirective
+      ],
+
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TestComponent);
+    component = fixture.debugElement.componentInstance;
+    el = fixture.debugElement;
+
+    fixture.detectChanges();
+  }));
   it('should create an instance', () => {
-    const directive = new ChangeColorDirective();
-    expect(directive).toBeTruthy();
+    expect(component).toBeTruthy();
+  });
+
+  it('should change the div background to red', () => {
+    const elements = el.query(By.directive(ChangeColorDirective));
+
+    expect(elements).toBeTruthy();
+
+    expect(elements.nativeElement.style.backgroundColor).toEqual('yellow');
+
+    const item = el.query(By.css('div:not([appChangeColor])'));
+    expect(item.nativeElement.style.backgroundColor).toEqual('');
   });
 });
